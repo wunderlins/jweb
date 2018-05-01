8bcaa965c1705354c1b3acd0c086857299283a11
Simon Wunderlin <swunderlin@gmail.com>
2018-05-01 09:09:16 +0200

    Added simple Generic Class.

---
+++ poc/java/generics/src/net/wunderlin/java/learning/generics/Classes.java
@@ -0,0 +1,23 @@
+package net.wunderlin.java.learning.generics;
+
+abstract class Animal {public String type = "";}
+class Mouse extends Animal {public Mouse() {type = "Mouse";}}
+class Bear  extends Animal {public Bear()  {type = "Bear";}}
+
+class Trap<T extends Animal> {
+	T trapped;
+	public void snare(T trapped) { this.trapped = trapped; }
+	public T release() {return trapped;}
+}
+
+public class Classes {
+	
+	public static void main(String[] args) {
+		
+		Trap<Mouse> mouseTrap = new Trap<>();
+		mouseTrap.snare(new Mouse());
+		Animal mouse = mouseTrap.release();
+		System.out.println("We cought a " + mouse.type);
+	}
+
+}
